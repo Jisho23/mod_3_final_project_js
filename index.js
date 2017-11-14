d70187eb820c1752da1dd419b8372c50a6c5df76
github username <github email address>
2017-11-14 11:26:00 -0500

    added constant urls for local host

---
+++ index.js
@@ -0,0 +1,3 @@
+const localHostCharacters = "http://localhost:3000/api/v1/characters";
+const localHostMonsters = "http://localhost:3000/api/v1/monsters";
+const localHostHighScore = "http://localhost:3000/api/v1/highscores";
